64df31260de52de2ca19ea7c6bfe695fd0e9acd5
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-28 21:22:20 -0400

    Added barebones landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import './LandingPage.css';
+
+function LandingPage(){
+    
+    return (
+        <div id="landing-page">
+            <h1 id="title">Welcome to GameReview</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
