f903c182242eadbca82d59eb8892b182238ca262
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-28 22:32:00 -0400

    Added button that will link to login page.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,17 @@
 import './LandingPage.css';
+import {Link} from 'react-router-dom';
 
 function LandingPage(){
     
     return (
         <div id="landing-page">
+            
+            
+            <Link to="/login&signup" id="link">
+                <button id="linkButton">Get Started</button>
+            </Link>
             <h1 id="title">Welcome to GameReview</h1>
+           
         </div>
     );
 }
