074e99dceb0e777a7b1e29878a58d69d8bc6e32f
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-06-30 16:31:12 -0400

    First user object attribute added

--- APIstuff/usermodel.js
+++ APIstuff/usermodel.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+const userObject = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+
+});
