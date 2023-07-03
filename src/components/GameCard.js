6f88694715b683397463c7ffe2650c4041fc747f
KOYA <ryanskolano@gmail.com>
2023-07-03 17:26:27 -0400

    -added icons directory -started on games page html/css -tweaked navbar css to prevent element overlapping -added components for game page -changed file names to prevent confusion with id's and classes

---
+++ src/components/GameCard.js
@@ -0,0 +1,11 @@
+function GameCard(){
+    return (
+        <div>   
+            <div class="item1">item1</div>
+            <div class="item2">item2</div>
+            <div class="item3">item3</div>
+        </div>
+    )
+}
+
+export default GameCard;
