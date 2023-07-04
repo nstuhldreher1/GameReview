39318a5f061b9ee5c8824b25df94ee81b02794df
KOYA <ryanskolano@gmail.com>
2023-07-04 17:54:52 -0400

    -added font family to GameCard -added reminder for scroll bar in review ReviewList -added user components for search pages -addded search icon -began working on search page for both users and games

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -3,6 +3,7 @@ import './ReviewList.css';
 
 function ReviewList(){
     /* list that contains Game Review components. This list will be returned to GamePage */
+    /* need to add scroll bar later on */
     return (
         <div id="reviewContainer">
             <GameReview/>
