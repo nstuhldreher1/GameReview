02d706dcdc1d3150b2c739430a5e1b682d6e9126
KOYA <ryanskolano@gmail.com>
2023-07-05 17:00:11 -0400

    -added tabs for users and games

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,16 @@ import './Search.css';
 import NavBar from '../components/NavBar.js';
 import searchIcon from '../images/search.png';
 
+import { useState } from 'react';
+
 function Search(){
+
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (tabType) => {
+        setToggleState(tabType);
+    };
+
     return (
         <div id="searchPage">
             <div id="navigation">
@@ -14,14 +23,10 @@ function Search(){
                 </button>
                 <input id="searchInput"></input>
             </form>
-            <button id="users">Users</button>
-            <button id="games">Games</button>
-            <div id="gameTab">
+            <button id="users" onClick={() => toggleTab(0)} style={{ textDecoration: toggleState ? 'none' : 'underline' }}>Users</button>
+            <button id="games" onClick={() => toggleTab(1)} style={{ textDecoration: toggleState ? 'underline' : 'none' }}>Games</button>
+            {toggleState === 1 ? <div id="userTab"></div> : <div id="gameTab"></div>}
 
-            </div>
-            <div id="userTab">
-                
-            </div>
         </div>
     );
 }
