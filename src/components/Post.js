7f8ba8dfbaefa0f12f9f3e4aead042fa79037794
JosephR4 <118718715+JosephR4@users.noreply.github.com>
2023-07-17 13:54:53 -0400

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,42 +1,17 @@
 import './Post.css';
-import fullstar from '../images/fullstar.svg';
-import emptystar from '../images/emptystar.svg';
+import placeHolder from '../images/placeHolderImage.png';
+import StarRating from './StarRating';
 
 function Post(props){
-    /*insert stars function
-    let count = 0;
-    let full = document.createElement("img");
-    let empty = document.createElement("img");
-
-    full.src = fullstar;
-    full.alt = "fullstar";
-    full.className = "star";
-
-    empty.src = emptystar;
-    empty.alt = "emptystar";
-    empty.className = "star";
-
-    for(let i = 0; i < 5; i++){
-        
-        if(count < props.stars){
-            document.getElementById("stars").appendChild(full);
-        }
-        else{
-            document.getElementById("stars").appendChild(empty);
-        }
-
-        count++;
-    }
-    */
 
     return(
         <div id="post">
-            <p class= "post-text">{props.name} {"@" + props.username}</p>
+            <img id ="reviewerProfilePicture" src={placeHolder}></img>
+            <p id= "post-text">{props.name} {"@" + props.username} {"Reviewed " + props.game}</p>
             <div id= "stars">
-                
+                <StarRating/>
             </div>
-            <p class = "post-text">{props.review}</p>
-
+            <p id = "post-text-review">{props.review}</p>
         </div>
     );
 }
