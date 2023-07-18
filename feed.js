cb0ec998d0db2294ae162118c57b375a5212f902
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-07-18 12:14:01 -0400

    game to games

--- feed.js
+++ feed.js
@@ -1,4 +1,4 @@
 // feed file
 const express = require('express');
 const router = express.Router();
-const Game = require('../models/game');
+const Game = require('../models/games'); // accessing the games table in the 
