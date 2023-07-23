5ffa01deacc34f8dfd73ede40ec117c3992f7602
miguelperson <miguelbaca@knights.ucf.edu>
2023-07-23 17:16:08 -0400

    added search file for game searching

---
+++ search.js
@@ -0,0 +1,9 @@
+// search file
+const userModel = require('./usermodel');
+const reviewModel = require('./reviewschema');
+const gameschema = require('./gamaeschema');
+const express = require('express');
+// const router = express.Router();
+// const Game = require('../models/games'); // accessing the games table in the 
+const app = express();
+const cors = require('cors');
