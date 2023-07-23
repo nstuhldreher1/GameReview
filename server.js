09fd71ca98fc012b9c00f998f13efb399aaa4273
||-KOYA-|| <ryanskolano@gmail.com>
2023-07-23 00:37:32 -0400

    -moved headers

--- server.js
+++ server.js
@@ -23,6 +23,18 @@ app.set('port', (process.env.PORT || 3001));
 
 app.use(express.static(path.join(__dirname + "/frontend/build")));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
 require('dotenv').config();
 // Connect to MongoDB
@@ -361,18 +373,7 @@ app.post('/api/resetPassword', async (req, res) => {
   }
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
+
 
 
 
