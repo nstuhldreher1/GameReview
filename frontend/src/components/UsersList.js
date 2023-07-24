6ee7ec3e0631077c650ca6b3674848d3b5f760c2
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-07-24 15:46:14 -0400

    Added frontend for user search page

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,11 +1,21 @@
 import './UsersList.css';
 
 import User from './User.js';
+import { useContext } from 'react';
+import { SearchContext } from '../pages/Search';
 
 // returns to search page (users tab)
 function UsersList(){
+    const {users} = useContext(SearchContext);
     return(
         <div id="list">
+            {users.map((user)=>{
+                return(
+                    <Link to = {'/profile/${user.userId}'} style = {{textDecoration: 'none'}}> 
+                        <User username={user.username} name={user.name}/>
+                    </Link> 
+                );
+            })}
         </div>
     )
 }
