97a826ed85fac75251215b666a36d44947006035
miguelperson <miguelbaca@knights.ucf.edu>
2023-07-31 18:02:06 -0400

    commented out the userID attribute

--- usermodel.js
+++ usermodel.js
@@ -31,11 +31,11 @@ const userObject = new mongoose.Schema({ // reference as const User = require('.
         type: Number
     },
 
-    UserID: {
-        type: Number,
-        default: Math.floor((Math.random() * 10000)),
-        unique: true
-    },
+//   UserID: {
+//        type: Number,
+//        default: Math.floor((Math.random() * 10000)),
+//        unique: true
+//    },
 
     profilePicture: {
         type: String,
